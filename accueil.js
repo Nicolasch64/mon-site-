3c95d9ed97964b21dd8f99acb771620cab1f22b9
Nicolasch64 <nidichapelle2000@yahoo.fr>
2024-12-17 15:24:57 +0100

    projet fil rouge

--- accueil.js
+++ accueil.js
@@ -1,14 +1,17 @@
-let bonjour = document.getElementById("b1");
-let ajouter = document.getElementById("b2");
-
-bonjour.addEventListener("click", alerte);
-ajouter.addEventListener("click", ajout);
-
-function alerte() {
-  alert("Bonjour");
+function toggleLanguageMenu() {
+	const menu = document.getElementById("languageMenu");
+	menu.classList.toggle("show");
 }
-function ajout() {
-  let para = document.createElement("p");
-  para.textContent = "Paragraphe ajouté";
-  document.body.appendChild(para);
-}
+
+const choixlangue = document.getElementById("choixlangue");
+choixlangue.addEventListener("click", () => {
+	toggleLanguageMenu();
+});
+window.addEventListener("click", (event) => {
+	if (!event.target.matches("#choixlangue")) {
+		let languagemenu = document.getElementById("languageMenu");
+		if (languagemenu.classList.contains("show")) {
+			languagemenu.classList.remove("show");
+		}
+	}
+});
